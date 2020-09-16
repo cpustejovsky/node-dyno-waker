94d625be0197a7bb7a81c3a3f1f6d2da10ee746c
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-16 19:03:26 -0400

    Revert "small fix for twitter-bot feature of dyno-waker"
    This reverts commit ab16d2180624cd8219e2ce83d6af05564aea7f0a.

--- dynowaker.js
+++ dynowaker.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 const moment = require("moment");
-const tz = require("moment-timezone");
+const tz = require("moment-timezone")
 
 const isWakeTime = () => {
-  let now = moment().tz("America/New_York");
+  let now = moment().tz('America/New_York');
   let hoursAM = [6, 7, 8, 9, 10, 11];
   let hoursPM = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -17,11 +17,9 @@ const isWakeTime = () => {
   return isWakeAM || isWakePM;
 };
 
-const wakeDyno = async (urlPrefix, twitterBot) => {
+const wakeDyno = async (urlPrefix) => {
   try {
-    await axios.get(
-      twitterBot ? urlPrefix : `https://${urlPrefix}.herokuapp.com/`
-    );
+    await axios.get(`https://${urlPrefix}.herokuapp.com/`);
     console.log(`hit ${urlPrefix}`);
   } catch (error) {
     console.log(error);
@@ -40,31 +38,24 @@ const dynoWaker = async (type, ...args) => {
   if (type === "daily") {
     if (isWakeTime()) {
       console.log(`Hitting dynos at ${moment().tz('America/New_York').format("h:mm A")}`);
-      await asyncForEach(args, wakeDyno)
-      setTimeout(dynoWaker, 1000 * 60 * 60 * 24, ...args);
+      wakeDyno(...args)
+      setTimeout(dynoWaker, 1000 * 60 * 60 * 24, "twitter-bot", ...args);
       console.log(`Finished hitting dynos at ${moment().tz('America/New_York').format("h:mm A")}`);
     }
   } else {
     if (isWakeTime()) {
-      console.log(
-        `Hitting dynos at ${moment().tz("America/New_York").format("h:mm A")}`
-      );
-      await asyncForEach(args, wakeDyno);
+      console.log(`Hitting dynos at ${moment().tz('America/New_York').format("h:mm A")}`);
+      await asyncForEach(args, wakeDyno)
       setTimeout(dynoWaker, 1000 * 60 * 30, ...args);
-      console.log(
-        `Finished hitting dynos at ${moment()
-          .tz("America/New_York")
-          .format("h:mm A")}`
-      );
+      console.log(`Finished hitting dynos at ${moment().tz('America/New_York').format("h:mm A")}`);
     } else {
       console.log(
-        `going to sleep for 30 minutes since it's ${moment()
-          .tz("America/New_York")
-          .format("h:mm A")}`
+        `going to sleep for 30 minutes since it's ${moment().tz('America/New_York').format("h:mm A")}`
       );
       setTimeout(dynoWaker, 1000 * 60 * 30, ...args);
     }
   }
+
 };
 
 module.exports = dynoWaker;
